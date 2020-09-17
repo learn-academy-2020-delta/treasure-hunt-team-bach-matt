f33a0b3311d18e3438d7ffdc4b9077733084171e
mmullis07 <eddiemull311@gmail.com>
2020-09-16 17:03:44 -0700

    clutched the user stories

--- src/App.js
+++ src/App.js
@@ -6,24 +6,41 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [...Array(9).fill(" ")],
-      questionMark: "?",
-      tree: "tree"
+      squares: [...Array(9).fill("?")],
+      treasure: Math.floor(Math.random()* 8),
+      countdown: 5
     }
   }
 
-  handleChange = (currentIndex) => {
-    let { squares, tree } = this.state
+  handleChange = (i) => {
+    let { squares, treasure, countdown } = this.state
+    countdown --
+    if (i !== treasure && countdown === 0){
+        alert("Sorry you lost.")
+      } else if(i === treasure) {
+        squares[i] = "🏆"
+        setTimeout(function() {alert("You got it! Play again?");}, 300)
+      } else {
+        squares[i] = "🌴"
+      } 
+    console.log(squares)
+    this.setState({ squares: squares }) 
+    this.setState({countdown: countdown})
+  }
 
-    this.setState({ squares: "tree" })
+  handleReset = () => {
+    this.setState({squares:[...Array(9).fill("?")]})
+    this.setState({treasure:Math.floor(Math.random()* 8)})
+    this.setState({countdown: 5})
   }
 
   render(){
-    let { squares, questionMark, tree } = this.state
+    let { squares, questionMark } = this.state
     let squareUnits = squares.map((square, index) => {
       return(
         <Square
-        tree = { this.handleChange }
+        discover = { this.handleChange }
+        square = {square}
         questionMark = { questionMark }
         index = {index}
         key = {index}
@@ -33,9 +50,13 @@ class App extends Component{
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        <h4>COUNTER: {this.state.countdown}</h4>
         <div id="gameBoard">
           {squareUnits}
         </div>
+        <button onClick={this.handleReset}>
+          Reset
+        </button>
       </React.Fragment>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react'
 
 class Square extends Component{
 
-  assignIndex = () => {
-    this.props.handleChange(this.props.index)
-  }
-
   render(){
     let { index } = this.props
     return(
       <React.Fragment>
-        <div id="square" onClick = { this.props.tree }>
-          {this.props.questionMark}
+        <div id="square" onClick={ () => this.props.discover(index)}>
+          {this.props.square}
         </div>
       </React.Fragment>
     )
